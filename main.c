054066a7a086f542cb3a0b192c2a5dab6cbea154
fr343 <fr343@uowmail.edu.au>
2023-10-03 09:43:47 +0400

    int main() {     printf("Available items in the vending machine:\n");     printf("Select a beverage: \n");     printf("1. Tea\n");     printf("2. Coffee\n");     printf("3. Water\n");
        int choice;
        printf("Enter your choice (1/2/3): ");
        scanf("%d", &choice);
    
        if (choice == 1) {
            printf("You selected Tea.\n");
        } else if (choice == 2) {
            printf("You selected Coffee.\n");
        } else if (choice == 3) {
            printf("You selected Water.\n");
        } else {
            printf("Invalid choice. Please select a valid option (1/2/3).\n");
        }
    
        return 0;
    }

--- main.c
+++ main.c
@@ -2,14 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // Initialize items in the vending machine
+int main() {
+    printf("Available items in the vending machine:\n");
+    printf("Press 1 to select coffee, 2 to select tea or 3 to select water\n");
+    std::cout << "Select a beverage: \n";
+    std::cout << "1. Tea\n";
+    std::cout << "2. Coffee\n";
+    std::cout << "3. Water\n";
+
+    int choice;
+    std::cout << "Enter your choice (1/2/3): ";
+    std::cin >> choice;
+
+    if (choice == 1) {
+        std::cout << "You selected Tea.\n";
+    } else if (choice == 2) {
+        std::cout << "You selected Coffee.\n";
+    } else if (choice == 3) {
+        std::cout << "You selected Water.\n";
+    } else {
+        std::cout << "Invalid choice. Please select a valid option (1/2/3).\n";
+    }
+
+    return 0;
+}
+
+typedef struct {
+    char name[50];
+    double price;
+    int quantity;
+    int min_quantity;
+} Item;
+
 Item vendingMachine[] = {
-    {"Tea", 1.5, 10, 3},
-    {"Coffee", 1.0, 15, 5},
+    {"Coffee", 1.5, 10, 3},
+    {"Tea", 1.0, 15, 5},
     {"Water", 0.75, 20, 8}
 };
 
-int main() 
-{
-    printf("Available items in the vending machine:\n");
-}
+
+
